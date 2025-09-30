27d51c9d28cc678b2a537b5fe85039700bf5095c
CoolAmbast <83899058+CoolAmbast@users.noreply.github.com>
2025-09-30 17:06:19 +0530

    rebuilt!

--- src/Components/NavBar/TopBar.tsx
+++ src/Components/NavBar/TopBar.tsx
@@ -1,31 +1,35 @@
-import { Grid } from '@mui/material'
+import { Box } from '@mui/material'
 import PillNav from '../NavBar/PillNav'
 import logo from '../../assets/icon.png'
 
 function TopBar() {
   return (
-    <div>
-        <Grid sx={{ width: '100%' }}>
-            <nav className="nav-container">
-              <PillNav
-                logo={logo}
-                logoAlt="Company Logo"
-                items={[
-                  { label: 'Home', href: '/' },
-                  { label: 'About us', href: '/about' },
-                  { label: 'Legal', href: '/legal' },
-                  { label: 'Discord', href: '/discord' }
-                ]}
-                activeHref="/"
-                className="custom-nav"
-                ease="power2.easeOut"
-                baseColor="#000000"
-                pillColor="#ffffff"
-                hoveredPillTextColor="#ffffff"
-                pillTextColor="#000000" />
-            </nav>
-          </Grid>
-    </div>
+    <Box
+      component="nav"
+      sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        mb: { xs: 2, md: 4 }
+      }}
+    >
+      <PillNav
+        logo={logo}
+        logoAlt="Company Logo"
+        items={[
+          { label: 'Home', href: '/' },
+          { label: 'About us', href: '/about' },
+          { label: 'Legal', href: '/legal' },
+          { label: 'Discord', href: '/discord' }
+        ]}
+        activeHref="/"
+        className="custom-nav"
+        ease="power2.easeOut"
+        baseColor="#000000"
+        pillColor="#ffffff"
+        hoveredPillTextColor="#ffffff"
+        pillTextColor="#000000" />
+    </Box>
   )
 }
 
