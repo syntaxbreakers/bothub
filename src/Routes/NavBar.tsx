b96857adab75f0882974bcc42aba6949fef540dc
CoolAmbast <83899058+CoolAmbast@users.noreply.github.com>
2025-10-13 13:37:39 +0530

    forms started

--- src/Routes/NavBar.tsx
+++ src/Routes/NavBar.tsx
@@ -4,6 +4,7 @@ import Threads from "../Components/ThreadsBackground/Threads"
 import Legal from "../Pages/Legal/Legal"
 import AboutUs from "../Pages/Aboutus/AboutUs"
 import Home from "../Pages/Home/Home"
+import Commision from "../Pages/Client/commision"
 
 export const NavBarRoutes = () => {
   return (
@@ -14,6 +15,7 @@ export const NavBarRoutes = () => {
         <Route path="/bothub" element={<Home />} />
         <Route path="/bothub/aboutus" element={<AboutUs />} />
         <Route path="/bothub/legal" element={<Legal />} />
+        <Route path="/bothub/commision" element={<Commision/>}/>
       </Routes>
     </Router>
   )
